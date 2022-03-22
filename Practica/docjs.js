6000a1a60236973240cc82e496278c1eb4c4e184
Trebla96 <albertfajardo96@hotmail.com>
2022-03-22 17:19:52 +0100

    ok

---
+++ Practica/docjs.js
@@ -0,0 +1,23 @@
+// Create the video tag.
+var video = document.createElement("video");
+// Check if this video tag is supported. Feature detection!
+if (video.canPlayType) {
+    // Check for OGG support.
+    //if (video.canPlayType("video/ogg")) {
+    //    video.src = "video.ogg";
+    //}
+    // Check for mp4 support.
+
+    if (video.canPlayType("video/mp4")) {
+        video.src = "mivideo.mp4";
+    }
+    // Turn the controls on.
+    //video.setAttribute("controls", "controls");
+    video.setAttribute("autoplay", "autoplay");
+
+    document.body.appendChild(video);
+} else {
+    var div = document.createElement("div");
+    div.innerHtml = "Backup message shown if the browser does not support the video tag.";
+    document.body.appendChild(div);
+}
