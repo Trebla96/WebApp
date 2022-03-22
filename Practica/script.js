69aa94f29efe2bcdac272fad122511a9097f7570
Trebla96 <albertfajardo96@hotmail.com>
2022-03-22 22:39:44 +0100

    ok

---
+++ Practica/script.js
@@ -0,0 +1,32 @@
+// mas info:  https://blog.webdevsimplified.com/2020-11/class-list/
+const body = document.body;
+body.append("hi","he");
+
+// Creamos div
+const div = document.createElement("div");
+
+/*
+//diferencia?
+div.innerText = "este es el inner text de div";
+div.textContent = 'este es el texto textContent';
+
+//para incluir texto html:
+div.innerHTML = "<strong> texto </strong>"
+
+Pero mejor crear elementos nuevos
+*/
+
+const strong = document.createElement("strong");
+strong.setAttribute("id","idstrong");
+div.id = "iddediv";
+
+strong.innerText = "hey strong text";
+
+
+div.appendChild(strong);
+//div.appendChild(div)
+
+
+
+//Añadimos div a la pagina
+body.appendChild(div);
