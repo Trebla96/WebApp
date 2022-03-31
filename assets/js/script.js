de259f3309dbfe14f263c4cd33a896699f7c2d72
Trebla96 <albertfajardo96@hotmail.com>
2022-03-31 17:05:35 +0200

    carrusel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+/* Start Carrusel de itinerarios */
+
+$('#recipeCarousel').carousel({
+    interval: 10000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+
+/* Fin Carrusel itinerarios */
+
 function heroVideo() {
     // Create the video tag.
     const HeroVideo = document.createElement("video");
